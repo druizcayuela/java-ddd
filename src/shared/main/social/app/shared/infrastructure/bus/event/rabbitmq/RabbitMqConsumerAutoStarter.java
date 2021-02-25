90a340e65b6632725dbcaaca1121692c4b4d4598
Daniel Ruiz Cayuela <druiz@financialforce.com>
2021-02-25 09:31:51 +0100

    UPDATE to doing some tests

---
+++ src/shared/main/social/app/shared/infrastructure/bus/event/rabbitmq/RabbitMqConsumerAutoStarter.java
@@ -0,0 +1,21 @@
+package social.app.shared.infrastructure.bus.event.rabbitmq;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import social.app.shared.domain.Service;
+
+@Service
+public class RabbitMqConsumerAutoStarter implements ApplicationListener {
+    private final RabbitMqDomainEventsConsumer rabbitMqDomainEventsConsumer;
+    public RabbitMqConsumerAutoStarter(RabbitMqDomainEventsConsumer rabbitMqDomainEventsConsumer) {
+        this.rabbitMqDomainEventsConsumer = rabbitMqDomainEventsConsumer;
+    }
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if(event instanceof ApplicationReadyEvent) {
+            rabbitMqDomainEventsConsumer.consume();
+            System.out.println("RabbitMQ consumer has been started.");
+        }
+    }
+}
